b875b6e353239991818f182bb34153888bbe3b46
Hayet Fellah <hayet.fellah145@gmail.com>
2022-10-19 22:44:40 +0100

    Fetch likes data and display it for each show.

--- src/homepage/display.js
+++ src/homepage/display.js
@@ -1,12 +1,15 @@
 import renderPopup from '../commentsPopup/modules/displayPopup.js';
 
-const url = 'https://api.tvmaze.com/shows';
+const tvApiUrl = 'https://api.tvmaze.com/shows';
+const involvementApiUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/kUJtIKt0WlDGnehZIL7s/likes';
 
 const showsDiv = document.querySelector('.shows');
 const previous = document.querySelector('.previous');
 const next = document.querySelector('.next');
 const pageNum = document.querySelector('.page-number');
 const body = document.querySelector('body');
+
+let showsArray = [];
 
 const getShowsData = async (url) => {
   const result = await fetch(url);
@@ -25,9 +28,16 @@ function createShowCard(obj) {
         <button>Reservations</button>
         <button class="comments">Comments</button>
       </div>
-        <button class="like-btn"><i class="fa-regular fa-heart" ></i></button>
+      <div class="like-section">
+      <button class="like-btn"><i class="fa-regular fa-heart" ></i></button>  
+      <span class="likes-number">0</span>
+      </div>
     </div>
   `;
+  const likesNumber = div.querySelector('.likes-number');
+  if ("likes" in obj){
+    likesNumber.innerHTML = obj.likes;
+  }
 
   const commentsBtn = div.querySelector('.comments');
   commentsBtn.addEventListener('click', ()=>{
@@ -39,40 +49,50 @@ function createShowCard(obj) {
 }
 
 const displayShows = (shows, pageNumber) => {
-  shows.splice(pageNumber * 10 - 10, 10).forEach((show) => {
+  shows.slice(pageNumber * 10 - 10, pageNumber * 10).forEach((show) => {
     const div = createShowCard(show);
     showsDiv.append(div);
   });
 };
 
-function loadNext(pageNumber) {
-  if (pageNumber + 1 < 25) {
-    pageNum.innerHTML = pageNumber + 1;
+function loadNext(pageNumber, shows) {
+  let nextPage = pageNumber + 1;
+  if (nextPage < 25) {
+    pageNum.innerHTML = nextPage;
     showsDiv.innerHTML = '';
-    getShowsData(url)
-      .then((response) => response.json())
-      .then((json) => displayShows(json, pageNumber + 1));
+    displayShows(shows, nextPage);
   }
 }
 
-function loadPrevious(pageNumber) {
-  if (pageNumber - 1 > 0) {
-    pageNum.innerHTML = pageNumber - 1;
+function loadPrevious(pageNumber, shows) {
+  let previousPage = pageNumber - 1;
+  if (previousPage > 0) {
+    pageNum.innerHTML = previous;
     showsDiv.innerHTML = '';
-    getShowsData(url)
-      .then((response) => response.json())
-      .then((json) => displayShows(json, pageNumber - 1));
+    displayShows(shows, previousPage);
   }
 }
 
 previous.addEventListener('click', () => {
-  loadPrevious(parseInt(pageNum.innerHTML, 10));
+  loadPrevious(parseInt(pageNum.innerHTML, 10), showsArray);
 });
 
 next.addEventListener('click', () => {
-  loadNext(parseInt(pageNum.innerHTML, 10));
+  loadNext(parseInt(pageNum.innerHTML, 10), showsArray);
 });
 
-getShowsData(url)
-  .then((response) => response.json())
-  .then((json) => displayShows(json, 1));
+getShowsData(tvApiUrl)
+  .then((response1) => response1.json())
+  .then((shows) => {
+    showsArray = shows;
+    getShowsData(involvementApiUrl)
+      .then((response) => response.json())
+      .then((likes) => {
+          likes.forEach((item, i)=>{
+            showsArray[i].likes =item.likes;
+          });
+          displayShows(showsArray,1);
+      });
+  });
+
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import './style.css';
-import './dummyData.js';
 import './commentsPopup/commentPopup.js';
 import './homepage/display.js';
