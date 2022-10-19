29ce01aa778d32a6ac336858da9eb617f258a43e
KareemWilson <kareemmagdywilson@gmail.com>
2022-10-19 17:22:49 +0200

    finish

--- src/reservationPopup/modules/renderPopup.js
+++ src/reservationPopup/modules/renderPopup.js
@@ -4,17 +4,21 @@ import "../reservationPopupStyles.css";
 const reservationPopup = (obj) => {
   const Popup = document.createElement("div");
   Popup.classList.add("popup");
-  Popup.innerHTML = `<div class='popup-container'>
+  Popup.innerHTML = `
+  <div class='popup-container'>
+  <img src=${obj.image.original} alt='Show Image' class='bg-img'>
     <i class='close-icon fa-regular fa-circle-xmark'></i>
     <div class='show'>
     <img src=${obj.image.medium} alt='Show Image' class='img'>
     <div class='show-details'>
       <p class='img-label'>${obj.name}</p>
       <ul class='details-grid'>
-          <li class='genres-title'>Genres<hr class='border'/></li>
+          <li class='details-section'>Genres<hr class='border'/></li>
           ${obj.genres
             .map((genre) => `<li class='genre-item'>${genre}</li>`)
             .join("")}
+            <li class='details-section'>Rating:   <span>${obj.rating.average}</span><hr class='border'/></li>
+            <li class='details-section'>Summary:   <hr class='border'/><span class='summary'>${obj.summary}</span></li>
       </ul>
     </div>
     </div>
