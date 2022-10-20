bb004d228199bf7289a3cf1f5c99331b4ebe2d04
Hayet Fellah <hayet.fellah145@gmail.com>
2022-10-20 17:32:36 +0100

    Fix linter errors.

--- src/homepage/display.js
+++ src/homepage/display.js
@@ -32,7 +32,7 @@ const likeShow = async (id, likesNumber, likesBtn) => {
       'Content-type': 'application/json; charset=UTF-8',
     },
   }).then(() => {
-    showsArray[id].likes = showsArray[id].likes + 1;
+    showsArray[id].likes += 1;
     likesNumber.innerHTML = parseInt(likesNumber.innerHTML, 10) + 1;
     const i = likesBtn.querySelector('i');
     i.classList.remove('fa-regular');
