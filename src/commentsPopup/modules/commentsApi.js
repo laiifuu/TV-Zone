0b26b69621682794599b56a9aa154955a9b6510a
Zuhaib Amjad <umar0423@gmail.com>
2022-10-20 16:00:28 +0500

    Fix linters errors

--- src/commentsPopup/modules/commentsApi.js
+++ src/commentsPopup/modules/commentsApi.js
@@ -1,6 +1,5 @@
 // post data to api
-const inolvementUrl =
-  'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/kUJtIKt0WlDGnehZIL7s/comments';
+const inolvementUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/kUJtIKt0WlDGnehZIL7s/comments';
 
 export const postCommentData = async (userName, comment, id) => {
   const response = await fetch(`${inolvementUrl}`, {
@@ -9,7 +8,7 @@ export const postCommentData = async (userName, comment, id) => {
     body: JSON.stringify({
       item_id: id,
       username: userName,
-      comment: comment,
+      comment,
     }),
   });
   return response.status;
