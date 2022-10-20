7825545e03aab4ed10a1c092db16a386fb92d0ee
Zuhaib Amjad <umar0423@gmail.com>
2022-10-20 19:57:26 +0500

    Fix linters errors

--- src/commentsPopup/modules/commentsCounter.test.js
+++ src/commentsPopup/modules/commentsCounter.test.js
@@ -22,40 +22,40 @@ describe('testing comments counter function', () => {
     const expectedResult = 3;
     // Assert
     expect(result).toBe(expectedResult);
-  }),
-    it('Empty array should result 0 number', () => {
-      // Arrange
-      const arr = [];
-      // Act
-      const result = commentsCount(arr);
-      const expectedResult = 0;
-      // Assert
-      expect(result).toBe(expectedResult);
-    }),
-    it('should return the length of the given array', () => {
-      // Arrange
-      const arr = [
-        {
-          userName: 'Tolkien',
-          comment: 'This show is amazing',
-        },
-        {
-          userName: 'Rollin',
-          comment: 'Who is the main lead Actor in this show',
-        },
-        {
-          userName: 'Smith',
-          comment: 'I would like to watch this show soon',
-        },
-        {
-          userName: 'Tommy',
-          comment: 'The best show ever',
-        },
-      ];
-      // Act
-      const result = commentsCount(arr);
-      const expectedResult = arr.length;
-      // Assert
-      expect(result).toBe(expectedResult);
-    });
+  });
+  it('Empty array should result 0 number', () => {
+    // Arrange
+    const arr = [];
+    // Act
+    const result = commentsCount(arr);
+    const expectedResult = 0;
+    // Assert
+    expect(result).toBe(expectedResult);
+  });
+  it('should return the length of the given array', () => {
+    // Arrange
+    const arr = [
+      {
+        userName: 'Tolkien',
+        comment: 'This show is amazing',
+      },
+      {
+        userName: 'Rollin',
+        comment: 'Who is the main lead Actor in this show',
+      },
+      {
+        userName: 'Smith',
+        comment: 'I would like to watch this show soon',
+      },
+      {
+        userName: 'Tommy',
+        comment: 'The best show ever',
+      },
+    ];
+    // Act
+    const result = commentsCount(arr);
+    const expectedResult = arr.length;
+    // Assert
+    expect(result).toBe(expectedResult);
+  });
 });
