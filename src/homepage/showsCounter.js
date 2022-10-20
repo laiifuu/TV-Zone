7e9e5bedab16f5988fff7d57fc2b01f5f6899c7f
Hayet Fellah <hayet.fellah145@gmail.com>
2022-10-20 15:15:14 +0100

    Add counter functions.

---
+++ src/homepage/showsCounter.js
@@ -0,0 +1,12 @@
+export function countDisplayedShows(showsDiv) {
+  let arr = Array.from(showsDiv.querySelectorAll('.show-card'));
+  return {
+    count: arr.length,
+    firstId: parseInt(arr[0].id, 10),
+    lastId: parseInt(arr[9].id, 10),
+  };
+}
+
+export function countShows(arr){
+    return arr.length;
+}
